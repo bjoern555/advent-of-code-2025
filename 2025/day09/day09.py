c903212a17fa73b384fddd2f980e6538c192078a
bjoern555 <bjoern-wagner@outlook.com>
2025-12-10 00:43:54 +0800

    day09: Solve second star (could not have solved part2 without tipps for the cross-checks)

--- 2025/day09/day09.py
+++ 2025/day09/day09.py
@@ -1,3 +1,6 @@
+from colorama.initialise import wrap_stream
+
+
 def read_input(file_path):
     with open(file_path, "r") as f:
         return f.read().strip()
@@ -36,7 +39,67 @@ def solve_part1(data):
 
 
 def solve_part2(data):
-    return "sol2"
+    points = []
+
+    for line in data:
+        col, row = line.split(",")
+        points.append((int(col), int(row)))
+
+    green_walls = []
+
+    for i in range(len(points) - 1):
+        green_walls.append((points[i], points[i + 1]))
+
+    green_walls.append((points[-1], points[0]))
+
+    max_area = 0
+
+    for i in range(len(points)):
+        for j in range(len(points)):
+            if i == j:
+                continue
+
+            w_p1 = points[i]
+            w_p2 = points[j]
+
+            r_y_min = min(w_p1[1], w_p2[1])
+            r_y_max = max(w_p1[1], w_p2[1])
+            r_x_min = min(w_p1[0], w_p2[0])
+            r_x_max = max(w_p1[0], w_p2[0])
+
+            valid = True
+
+            for p1, p2 in green_walls:
+                w_x1, w_y1 = p1
+                w_x2, w_y2 = p2
+
+                w_x_min, w_x_max = min(w_x1, w_x2), max(w_x1, w_x2)
+                w_y_min, w_y_max = min(w_y1, w_y2), max(w_y1, w_y2)
+
+                if w_y1 == w_y2:
+                    wall_y = w_y1
+                    if r_y_min < wall_y < r_y_max:
+                        if w_x_min < r_x_max and w_x_max > r_x_min:
+                            valid = False
+                            break
+
+                elif w_x1 == w_x2:
+                    wall_x = w_x1
+                    if r_x_min < wall_x < r_x_max:
+                        if w_y_min < r_y_max and w_y_max > r_y_min:
+                            valid = False
+                            break
+
+            if valid:
+                width = abs(w_p1[0] - w_p2[0]) + 1
+                height = abs(w_p1[1] - w_p2[1]) + 1
+
+                area = width * height
+
+                if area > max_area:
+                    max_area = area
+
+    return max_area
 
 
 if __name__ == "__main__":
